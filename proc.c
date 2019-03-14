c05cab795122f723459afb4aea3719e49c82f697
tallav <tal.lavon1@gmail.com>
2019-03-14 16:46:12 +0200

    exit signiture update

--- proc.c
+++ proc.c
@@ -235,7 +235,7 @@ fork(void)
 // An exited process remains in the zombie state
 // until its parent calls wait() to find out it exited.
 void
-exit(void)
+exit(int status)
 {
   struct proc *curproc = myproc();
   struct proc *p;
--- user.h
+++ user.h
@@ -3,7 +3,8 @@ struct rtcdate;
 
 // system calls
 int fork(void);
-int exit(void) __attribute__((noreturn));
+void exit(int status) __attribute__((noreturn));
+//int exit(void) __attribute__((noreturn));
 int wait(void);
 int pipe(int*);
 int write(int, const void*, int);
