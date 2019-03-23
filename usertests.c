9b661a3d765d049d36c13e2a2431a1ed399999a2
Tal <tal.lavon1@gmail.com>
2019-03-23 12:23:51 +0200

    usertest debug

--- usertests.c
+++ usertests.c
@@ -620,10 +620,9 @@ createdelete(void)
   }
 
   for(pi = 0; pi < 4; pi++){
-	printf(1, "entered wait\n");
     wait(0);
   }
-
+  printf(1, "after wait\n");
   name[0] = name[1] = name[2] = 0;
   for(i = 0; i < N; i++){
     for(pi = 0; pi < 4; pi++){
